3a39c85d1bc0663e8df3fe357e48af60cda22612
Colin Brevitz <verlander94@gmail.com>
2017-02-16 01:26:10 -0500

    Add header file for create_graph

--- create_graph.c
+++ create_graph.c
@@ -1,10 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include "create_graph.h"
 
-#define MAX_DEGREE_CONNECT 3
+int main(void) {
+  vertex ***graph = NULL;
+  graph = mallocGraph(graph);
 
-typedef unsigned char vertex;
+  srand(time(NULL));
+  int vertexes, edges;
+
+  for (vertexes = 0; vertexes < 10; vertexes++) {
+    printf("%d:\t", vertexes);
+    for (edges = 0; edges < 3; edges++) {
+      if (rand() % 2 == 1) {
+        int linkedVertex = rand() % 10;
+        graph[vertexes][edges] = *graph[linkedVertex];
+        printf("%d, ", linkedVertex);
+      } else
+        graph[vertexes][edges] = NULL;
+    }
+    printf("\n");
+   }
+   return 0;
+}
 
 vertex*** mallocGraph(vertex ***graph) {
   if ((graph = (vertex ***) malloc(sizeof(vertex **) * 10)) == NULL) {
@@ -28,26 +47,4 @@ vertex*** mallocGraph(vertex ***graph) {
     }
   }
   return graph;
-}
-
-int main(void) {
-  vertex ***graph = NULL;
-  graph = mallocGraph(graph);
-
-  srand(time(NULL));
-  int vertexes, edges;
-
-  for (vertexes = 0; vertexes < 10; vertexes++) {
-    printf("%d:\t", vertexes);
-    for (edges = 0; edges < 3; edges++) {
-      if (rand() % 2 == 1) {
-        int linkedVertex = rand() % 10;
-        graph[vertexes][edges] = *graph[linkedVertex];
-        printf("%d, ", linkedVertex);
-      } else
-        graph[vertexes][edges] = NULL;
-    }
-    printf("\n");
-   }
-   return 0;
 }
---
+++ create_graph.h
@@ -0,0 +1,10 @@
+#ifndef CREATE_GRAPH_H
+#define CREATE_GRAPH_H
+
+#define MAX_DEGREE_CONNECT 3
+typedef unsigned char vertex;
+
+int main(void);
+vertex*** mallocGraph(vertex ***graph);
+
+#endif
